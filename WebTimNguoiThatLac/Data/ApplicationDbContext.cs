83d0819b67c304086960fec948b63114b3dd7d45
DINHCONGMINH <dinhcongminh4424@gmail.com>
2025-04-16 10:53:53 +0700

    Add ApplicationDbContext class for Entity Framework Core integration

---
+++ WebTimNguoiThatLac/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebTimNguoiThatLac.Models;
+
+namespace WebTimNguoiThatLac.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<TimNguoi> TimNguois { get; set; }
+        public DbSet<AnhTimNguoi> AnhTimNguois { get; set; }
+        public DbSet<BinhLuan> BinhLuans { get; set; }
+        public DbSet<BaoCaoBinhLuan> BaoCaoBinhLuans { get; set; }
+        public DbSet<BaoCaoBaiViet> BaoCaoBaiViets { get; set; }
+        public DbSet<HopThoaiTinNhan> HopThoaiTinNhans { get; set; }
+        public DbSet<TinNhan> TinNhans { get; set; }
+        public DbSet<NguoiThamGia> NguoiThamGias { get; set; }
+        public DbSet<GioiThieu> GioiThieus { get; set; }
+        public DbSet<TinTuc> TinTucs { get; set; }
+        public DbSet<TrungBayHinhAnh> TrungBayHinhAnhs { get; set; }
+        public DbSet<TimThayNguoiThatLac> TimThayNguoiThatLacs { get; set; }
+        public DbSet<NguoiDungLienHe> NguoiDungLienHes { get; set; }
+        public DbSet<NhanChung> NhanChungs { get; set; }
+    }
+}

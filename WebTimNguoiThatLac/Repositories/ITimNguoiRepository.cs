37be32fdc3939460bb73ac3bf839b9c9e092d706
Hannie1812 <bichhang18122004@gmail.com>
2025-04-18 04:43:59 +0700

    Implement NhanChung creation with file upload and repository pattern; enhance UI with validation messages and success/error notifications

---
+++ WebTimNguoiThatLac/Repositories/ITimNguoiRepository.cs
@@ -0,0 +1,10 @@
+﻿using WebTimNguoiThatLac.Models;
+
+namespace WebTimNguoiThatLac.Repositories
+{
+    public interface ITimNguoiRepository
+    {
+        Task<IEnumerable<TimNguoi>> GetAllTimNguoiAsync();
+        Task<TimNguoi> GetTimNguoiByIdAsync(int id);
+    }
+}

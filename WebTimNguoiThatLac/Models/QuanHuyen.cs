80b2d21fc149af2241bb8099ba644f4d5ecc6d9f
Hannie1812 <bichhang18122004@gmail.com>
2025-05-01 02:01:53 +0700

    Add TinhThanh and QuanHuyen models; update ApplicationDbContext and TimNguoiController to use database values

---
+++ WebTimNguoiThatLac/Models/QuanHuyen.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebTimNguoiThatLac.Models
+{
+    [Table("QuanHuyen")]
+    public class QuanHuyen
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string TenQuanHuyen { get; set; }
+
+        [Required]
+        public int IdTinhThanh { get; set; }
+
+        [ForeignKey("IdTinhThanh")]
+        public TinhThanh TinhThanh { get; set; }
+
+        public ICollection<TimNguoi> TimNguois { get; set; } = new HashSet<TimNguoi>();
+    }
+}
---
+++ WebTimNguoiThatLac/Models/TinhThanh.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebTimNguoiThatLac.Models
+{
+    [Table("TinhThanh")]
+    public class TinhThanh
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string TenTinhThanh { get; set; }
+
+        public ICollection<QuanHuyen> QuanHuyens { get; set; } = new HashSet<QuanHuyen>();
+    }
+}
